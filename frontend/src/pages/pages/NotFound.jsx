4c2719d1c302f8445ded8d7902e9f7bb33e8190a
valiyevcilo <7bq70gr@code.edu.az>
2023-02-22 11:59:52 +0400

    new file added

--- frontend/src/pages/pages/NotFound.jsx
+++ frontend/src/pages/pages/NotFound.jsx
@@ -21,6 +21,7 @@ const NotFound = () => {
               <li className='slash'><NavLink style={({ isActive }) =>
                 isActive ? activeStyle : undefined
               } to='/*'>404</NavLink> </li>
+              
             </ul>
           </div>
         </div>
